1fa60175ab5ea99e68fc107c2ccece7ba90bd4f4
Mishkis <misha.o.gm@gmail.com>
2024-06-19 16:47:45 -0300

    Added highlighting by treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,16 @@
+local languages = { "c", "lua", "vim", "vimdoc", "query", "python", "c_sharp", "javascript"} 
+
 return {
      "nvim-treesitter/nvim-treesitter",
+     config = function()
+          require("nvim-treesitter.configs").setup({
+               ensure_installed = languages,
+
+               auto_install = true,
+
+               highlight = {
+                    enable = true,
+               }
+          })
+     end,
 }
