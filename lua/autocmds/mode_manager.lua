a09a7a7380875a6b1c2dadf38383244f4e5bc085
Mishkis <misha.o.gm@gmail.com>
2024-06-21 18:09:59 -0300

    Added custom colors and icons to the status line.

--- lua/autocmds/mode_manager.lua
+++ lua/autocmds/mode_manager.lua
@@ -3,16 +3,36 @@ local col = require("theme/colors")
 local changedHighlights = {
     ["NormalNC"] = { 0, 3 },
     ["Cursor"] = { 1, 2 },
-    ["WinSeparator"] = { 1, 2 },
+    ["WinSeparator"] = { 2, 2 },
     ["WinMode"] = { 1, 2 },
     ["WinModeTrans"] = { 1, 2 },
+    ["WinDir"] = {2, 1},
+}
+
+local icons = {
+    ["lua"] = "",
+    ["python"] = "",
+    ["tex"] = "󱗆",
+    ["javascript"] = "",
+    ["toml"] = "",
+    ["json"] = "",
+    ["jproperties"] = "",
+    ["text"] = "",
+    ["c"] = "",
+    ["cs"] = "",
 }
 
 local function ModeUpdate(textMode, colors)
     for targetHighlight, i in pairs(changedHighlights) do
         vim.api.nvim_set_hl(0, targetHighlight, {fg = colors[i[1]], bg = colors[i[2]]})
     end
-    vim.opt.stl="%#WinMode# %{winnr()} %-8.("..textMode.."%) %#WinModeTrans#│%#WinDir# %F %#Modified#%m%#WinDir#%=%-4.L %#WinModeTrans#│%#WinMode#   %-3.l%-3.(:%)%-3.c"
+
+    local icon = icons[vim.bo.filetype]
+    if icon == nil then
+        icon = ""
+    end
+
+    vim.opt_local.stl="%#WinMode# "..icon.." %{winnr()} %#WinMode#%-8.("..textMode.."%) %#WinModeTrans#%#WinDir# %F %#Modified#%m%#WinDir#%=%-4.L %#WinModeTrans#%#WinMode#   %-3.l%-3.(:%)%-3.c"
 end
 
 vim.api.nvim_create_autocmd("ModeChanged", {
