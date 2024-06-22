efb82d4dc4ac0e6f9e9619651994c49dd08f3674
Mishkis <misha.o.gm@gmail.com>
2024-06-22 01:35:15 -0300

    Added keybindings to lsp commands.

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -18,6 +18,10 @@ return {
     },
 
     config = function()
+        vim.keymap.set("n", "<Leader>h", vim.lsp.buf.hover, { desc = "[h]over word" })
+        vim.keymap.set("n", "<Leader>d", vim.lsp.buf.definition, { desc = "Goto [d]efinition" })
+        vim.keymap.set("n", "<Leader>td", vim.lsp.buf.type_definition, { desc = "Goto [t]ype [d]efiniton" })
+
         require("mason-lspconfig").setup({
             ensure_installed = vim.tbl_keys(servers)
         })
