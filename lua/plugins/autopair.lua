04c398b9726e4ff4c484e4e2b03d516b8e367562
Mishkis <misha.o.gm@gmail.com>
2024-06-28 16:44:19 -0300

    Added nvim-surround.

--- lua/plugins/autopair.lua
+++ lua/plugins/autopair.lua
@@ -1,12 +1,21 @@
 return {
-   "altermo/ultimate-autopair.nvim",
-   config = function()
-       require("ultimate-autopair").setup({
-           fastwarp = {
-               faster = true,
-               map = "<C-w>",
-               rmap = "<C-g>",
-           }
-       })
-   end
+    {
+        "altermo/ultimate-autopair.nvim",
+        config = function()
+            require("ultimate-autopair").setup({
+                fastwarp = {
+                    faster = true,
+                    map = "<C-w>",
+                    rmap = "<C-g>",
+                }
+            })
+        end
+    },
+    {
+        "kylechui/nvim-surround",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({})
+        end
+    }
 }
